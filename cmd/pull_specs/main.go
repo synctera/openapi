66c7177034d755a4abec9471a586ec6020d54739
Jamie Markle <jamie@synctera.com>
2021-06-15 15:11:18 -0400

    pull in specs and generate docs and code
    remove checked-in specs, code and merge configs
    add pipeline stages that produce these as artifacts

--- cmd/pull_specs/main.go
+++ cmd/pull_specs/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/go-git/go-billy/v5"
@@ -11,26 +12,70 @@ import (
 	"io"
 	"log"
 	"os"
+	"os/exec"
 	"path"
 	"strings"
 	"time"
 )
 
+type multiStringFlag []string
+
+func (flag multiStringFlag) String() string {
+	return strings.Join(flag, ",")
+}
+
+func (flag *multiStringFlag) Set(value string) error {
+	*flag = append(*flag, value)
+	return nil
+}
+
 func main() {
 	outputBasePath := flag.String("output", "", "Base output path for OpenAPI specification files")
+	var projects multiStringFlag
+	var internalApis multiStringFlag
+
+	flag.Var(&projects, "project", "project name, use this multiple times for multiple projects")
+	flag.Var(&internalApis, "internal", "API endpoint name to be considered internal only (eg signups)")
 	flag.Parse()
+
 	if outputBasePath == nil || len(*outputBasePath) == 0 {
-		fmt.Println("Must specify \"output\" parameter.")
-		os.Exit(1)
+		log.Fatalln("Must specify \"output\" parameter.")
+	}
+	if len(projects) == 0 {
+		log.Fatalln("Must specify at least one \"project\" parameter.")
 	}
 
+	entityName := func(apiRootSpec string) string {
+		return path.Base(path.Dir(apiRootSpec))
+	}
+
+	isInternal := func(apiRootSpec string) bool {
+		entity := entityName(apiRootSpec)
+		for _, internalApi := range internalApis {
+			if internalApi == entity {
+				return true
+			}
+		}
+		return false
+	}
+
+	const openapiDirName = "openapi"
+	outputFileName := func(inputFileName string) string {
+		return path.Join(*outputBasePath, strings.TrimPrefix(inputFileName, openapiDirName))
+	}
+
+	gitUrlPrefix := "git@gitlab.com:synctera/"
+	if ciToken := os.Getenv("CI_JOB_TOKEN"); len(ciToken) > 0 {
+		gitUrlPrefix = fmt.Sprintf("https://gitlab-ci-token:%s@gitlab.com/synctera/", ciToken)
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	for _, project := range []string{"mainapi"} {
+	var specFiles specSearchResults
+	for _, project := range projects {
 		repo, err := git.CloneContext(ctx, memory.NewStorage(), memfs.New(), &git.CloneOptions{
-			URL:          "git@gitlab.com:synctera/" + project,
+			URL:          gitUrlPrefix + project,
 			SingleBranch: true,
 			Depth:        1,
 		})
@@ -49,32 +94,89 @@ func main() {
 			log.Fatalf("Error getting working tree: %s", err)
 		}
 
-		const openapiDirName = "openapi"
-		specFileNames := findSpecs(tree.Filesystem, openapiDirName)
+		projectSpecFiles := findSpecs(tree.Filesystem, openapiDirName)
+		log.Printf("Found %d root API files (%d individual files)", len(projectSpecFiles.apiRoots), len(projectSpecFiles.all))
 
-		for _, specFileName := range specFileNames {
-			input, err := tree.Filesystem.Open(specFileName)
+		for _, specFileName := range projectSpecFiles.all {
+			inputFile, err := tree.Filesystem.Open(specFileName)
 			if err != nil {
 				log.Fatalf("Error opening %s: %s", specFileName, err)
 			}
 
-			outputFileName := path.Join(*outputBasePath, strings.TrimPrefix(specFileName, openapiDirName))
-			os.MkdirAll(path.Dir(outputFileName), os.ModePerm)
-
-			output, err := os.Create(outputFileName)
+			output := outputFileName(specFileName)
+			if err := os.MkdirAll(path.Dir(output), os.ModePerm); err != nil {
+				log.Fatalf("Error creating directory for %s: %s", output, err)
+			}
+			outputFile, err := os.Create(output)
 			if err != nil {
-				log.Fatalf("Error creating %s: %s", outputFileName, err)
+				log.Fatalf("Error creating %s: %s", output, err)
 			}
-			if _, err := io.Copy(output, input); err != nil {
-				log.Fatalf("Error copying from %s to %s: %s", specFileName, outputFileName, err)
+
+			if _, err := io.Copy(outputFile, inputFile); err != nil {
+				log.Fatalf("Error copying from %s to %s: %s", specFileName, output, err)
 			}
+		}
+
+		specFiles.join(projectSpecFiles)
+	}
+
+	log.Printf("TOTAL %d root API files (%d individual files)", len(specFiles.apiRoots), len(specFiles.all))
+
+	{
+		var externalApiRoots []string
+		var internalApiRoots []string
+		for _, apiRoot := range specFiles.apiRoots {
+			bundled := fmt.Sprintf("%s-api-bundled.yml", entityName(apiRoot))
+
+			internalApiRoots = append(internalApiRoots, bundled)
+			if isInternal(apiRoot) {
+				log.Println("-", apiRoot, "(internal only)")
+			} else {
+				log.Println("-", apiRoot)
+				externalApiRoots = append(externalApiRoots, bundled)
+			}
+		}
+
+		// write JSON config file for openapi-merge-cli to create combined internal and external specs
+		// NB: openapi-merge-cli assumes relative paths so don't include outputBasePath
+		writeMergeConfig(internalApiRoots, "internal-api-merged-bundled.yml", path.Join(*outputBasePath, "merge-internal-apis.json"))
+
+		// write JSON config file for openapi-merge-cli to create combined external spec
+		writeMergeConfig(externalApiRoots, "external-api-merged-bundled.yml", path.Join(*outputBasePath, "merge-external-apis.json"))
+	}
+
+	// bundle all API roots so they are self-contained and can be merged to the combined specs
+	for _, apiRoot := range specFiles.apiRoots {
+		input := outputFileName(apiRoot)
+		bundledOutput := path.Join(*outputBasePath, fmt.Sprintf("%s-api-bundled.yml", entityName(apiRoot)))
+
+		log.Println("bundling", input, "->", bundledOutput)
+		command := exec.Command("openapi", "bundle", input, "--ext", "yml", "--output", bundledOutput)
+
+		if err := command.Run(); err != nil {
+			log.Fatalf("Error bundling %s: %s", input, err)
 		}
 	}
 }
 
-func findSpecs(fileSystem billy.Filesystem, dirPath string) []string {
-	var specFileNames []string
+type specSearchResults struct {
+	all      []string
+	apiRoots []string
+}
 
+func (r *specSearchResults) add(specPath string) {
+	r.all = append(r.all, specPath)
+	if path.Base(specPath) == "api.yml" || path.Base(specPath) == "api.yaml" {
+		r.apiRoots = append(r.apiRoots, specPath)
+	}
+}
+
+func (r *specSearchResults) join(other specSearchResults) {
+	r.all = append(r.all, other.all...)
+	r.apiRoots = append(r.apiRoots, other.apiRoots...)
+}
+
+func findSpecs(fileSystem billy.Filesystem, dirPath string) (results specSearchResults) {
 	dirEntries, err := fileSystem.ReadDir(dirPath)
 	if err != nil {
 		log.Fatalf("Failed to read directory %s: %s", dirPath, err)
@@ -88,7 +190,7 @@ func findSpecs(fileSystem billy.Filesystem, dirPath string) []string {
 			if dirEntry.Name() == "gen" {
 				continue
 			}
-			specFileNames = append(specFileNames, findSpecs(fileSystem, fullPath)...)
+			results.join(findSpecs(fileSystem, fullPath))
 		} else if dirEntry.Mode().IsRegular() {
 			// append full path to results if name ends with .yml or .yaml and name is not openapi.yml
 			if !strings.HasSuffix(name, ".yml") && !strings.HasSuffix(name, ".yaml") {
@@ -97,9 +199,37 @@ func findSpecs(fileSystem billy.Filesystem, dirPath string) []string {
 			if name == "openapi.yml" {
 				continue // top level openapi.yml is a generated file of all specs combined (for kin router ingestion)
 			}
-			specFileNames = append(specFileNames, fullPath)
+			results.add(fullPath)
 		}
 	}
+	return
+}
 
-	return specFileNames
+// MergeConfig is compatible with openapi-merge-cli
+type MergeConfig struct {
+	Inputs []MergeConfigInput `json:"inputs"`
+	Output string             `json:"output"`
+}
+
+type MergeConfigInput struct {
+	InputFile string `json:"inputFile"`
+}
+
+func writeMergeConfig(inputs []string, output string, config string) {
+	configFile, err := os.Create(config)
+	if err != nil {
+		log.Fatalf("Error creating %s: %s", config, err)
+	}
+
+	var mergeConfig MergeConfig
+	for _, input := range inputs {
+		mergeConfig.Inputs = append(mergeConfig.Inputs, MergeConfigInput{InputFile: input})
+	}
+	mergeConfig.Output = output
+
+	encoder := json.NewEncoder(configFile)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(mergeConfig); err != nil {
+		log.Fatalf("Error encoding %s: %s", config, err)
+	}
 }
